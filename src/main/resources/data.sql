d29d9fdd1d1d2bc33dd31490f09d6f93d6156a51
DESKTOP-0SLF34B\weavus <test@gmail.com>
2022-11-18 17:03:23 +0900

    지적대응

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,5 @@
 insert into customer (name,address) values('aaa', 'Lastname');
 insert into customer (name,address) values('bbb', 'Lastname');
-insert into customer (name,address) values('ccc', 'Lastname');
+insert into customer (name,address) values('ccc11', 'Lastname');
 insert into customer (name,address) values('ddd', 'Lastname');
 insert into customer (name,address) values('eee', 'Lastname');
-insert into customer (name,address) values('fff', 'Lastname');
