4e60869a967fdef722e6e4d1b9075f7357d39fe4
DESKTOP-0SLF34B\weavus <test@gmail.com>
2022-11-18 16:56:43 +0900

    데이터추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 insert into customer (name,address) values('aaa', 'Lastname');
 insert into customer (name,address) values('bbb', 'Lastname');
 insert into customer (name,address) values('ccc', 'Lastname');
+insert into customer (name,address) values('ddd', 'Lastname');
+insert into customer (name,address) values('eee', 'Lastname');
+insert into customer (name,address) values('fff', 'Lastname');
